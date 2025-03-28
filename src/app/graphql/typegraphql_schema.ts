ba62d6d2d33ed705adfa56e816dc09ca3d347fed
Arijit Das [Itobuz] <arijit@itobuz.com>
2025-03-28 12:45:30 +0530

    feat: init

--- src/app/graphql/typegraphql_schema.ts
+++ src/app/graphql/typegraphql_schema.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { buildSchema } from 'type-graphql';
 import { Container } from 'typedi';
 import { HealthResolver } from './health/health.resolver.js';
@@ -5,5 +6,5 @@ import { HealthResolver } from './health/health.resolver.js';
 export const typegraphql_schema = await buildSchema({
   resolvers: [HealthResolver],
   container: Container,
-  emitSchemaFile: { path: 'src/app/graphql/schema.graphql', sortedSchema: true },
+  emitSchemaFile: { path: path.join(process.cwd(), 'src', 'app', 'graphql', 'schema.graphql'), sortedSchema: true },
 });
