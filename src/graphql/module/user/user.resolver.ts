b45132b13ba2387b4165e00abcf267d9bcb9f4ef
Arijit Das [Itobuz] <arijit@itobuz.com>
2025-04-04 18:50:14 +0530

    feat: file upload

--- src/graphql/module/user/user.resolver.ts
+++ src/graphql/module/user/user.resolver.ts
@@ -1,7 +1,10 @@
 import { Role } from '@prisma/client';
-import { Authorized, Ctx, Query, Resolver } from 'type-graphql';
+import GraphQLUpload, { type FileUpload } from 'graphql-upload/GraphQLUpload.mjs';
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { config } from '../../../config/config.js';
 import { AuthorizedRole } from '../../decorator/authorizedRole.decorator.js';
 import type { ApolloGraphqlContext } from '../../interface/apolloGraphqlContext.interface.js';
+import { GraphQLBaseResponse } from '../../lib/graphqlBaseResponse.lib.js';
 import { GetUserProfileOutput } from './dto/getUserProfile.output.js';
 import { UserService } from './user.service.js';
 
@@ -14,5 +17,14 @@ export class UserResolver {
   @Query(() => GetUserProfileOutput)
   async getUserProfile(@Ctx() ctx: ApolloGraphqlContext): Promise<GetUserProfileOutput> {
     return await this.userService.getUserProfile(ctx);
+  }
+
+  @Mutation(() => GraphQLBaseResponse)
+  async uploadUserProfileImage(@Arg('file', () => GraphQLUpload) file: FileUpload): Promise<GraphQLBaseResponse> {
+    return {
+      environment: config.app.APP_ENV,
+      success: true,
+      message: 'uploadUserProfileImage',
+    };
   }
 }
